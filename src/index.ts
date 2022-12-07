228d8b9226c367d0645916e00c36a493d5aad026
Vincent Sjögren <vincentsjogren@gmail.com>
2022-12-07 13:11:15 +0100

    updated templates

--- src/index.ts
+++ src/index.ts
@@ -8,142 +8,143 @@ import minimist from 'minimist';
 import prompts from 'prompts';
 import { green, red, reset, yellow } from 'kolorist';
 import {
-	copy,
-	formatTargetDir,
-	emptyDir,
-	isEmpty,
-	isValidPackageName,
-	toValidPackageName,
-	matchesPatterns,
+    copy,
+    formatTargetDir,
+    emptyDir,
+    isEmpty,
+    isValidPackageName,
+    toValidPackageName,
 } from './utils';
 
 const argv = minimist<{
-	t?: string;
+    t?: string;
 }>(process.argv.slice(2), { string: ['_'] });
 const cwd = process.cwd();
 
 (async function main() {
-	let targetDir = formatTargetDir(argv._[0]);
-	const defaultTargetDir = 'my-microbundle-project';
+    let targetDir = formatTargetDir(argv._[0]);
+    const defaultTargetDir = 'my-microbundle-project';
 
-	const getProjectName = () =>
-		!targetDir ? path.basename(path.resolve()) : targetDir;
+    const getProjectName = () =>
+        !targetDir ? path.basename(path.resolve()) : targetDir;
 
-	let result: prompts.Answers<
-		'projectName' | 'overwrite' | 'packageName' | 'useTypeScript'
-	>;
-	try {
-		result = await prompts(
-			[
-				{
-					type: targetDir ? null : 'text',
-					message: reset('Project name:'),
-					initial: defaultTargetDir,
-					onState: ({ value }) => {
-						targetDir = formatTargetDir(value) || defaultTargetDir;
-					},
-					name: 'projectName',
-				},
-				{
-					type: () =>
-						!targetDir ||
-						!fs.existsSync(targetDir) ||
-						isEmpty(targetDir)
-							? null
-							: 'confirm',
-					message: () =>
-						(targetDir === '.'
-							? 'Current directory'
-							: `Target directory "${targetDir}"`) +
-						` is not empty. Remove existing files and continue? `,
-					name: 'overwrite',
-				},
-				{
-					type: (_, { overwrite }) => {
-						if (overwrite !== false) return null;
-						throw new Error(`${red('✖')} Operation cancelled`);
-					},
-					name: 'overwriteChecker',
-				},
-				{
-					type: () =>
-						isValidPackageName(getProjectName()) ? null : 'text',
-					name: 'packageName',
-					message: reset('Package name:'),
-					initial: () => toValidPackageName(getProjectName()),
-					validate: (dir) =>
-						isValidPackageName(dir) || 'Invalid package.json name',
-				},
-				{
-					type: 'confirm',
-					name: 'useTypeScript',
-					message: 'Use TypeScript? ',
-				},
-			],
-			{
-				onCancel: () => {
-					throw new Error(`${red('✖')} Operation cancelled`);
-				},
-			}
-		);
-	} catch (err: any) {
-		console.log(err.message);
-		return;
-	}
+    let result: prompts.Answers<
+        'projectName' | 'overwrite' | 'packageName' | 'useTypeScript'
+    >;
+    try {
+        result = await prompts(
+            [
+                {
+                    type: targetDir ? null : 'text',
+                    message: reset('Project name:'),
+                    initial: defaultTargetDir,
+                    onState: ({ value }) => {
+                        targetDir = formatTargetDir(value) || defaultTargetDir;
+                    },
+                    name: 'projectName',
+                },
+                {
+                    type: () =>
+                        !targetDir ||
+                        !fs.existsSync(targetDir) ||
+                        isEmpty(targetDir)
+                            ? null
+                            : 'toggle',
+                    message: () =>
+                        (targetDir === '.'
+                            ? 'Current directory'
+                            : `Target directory "${targetDir}"`) +
+                        ` is not empty. Remove existing files and continue? `,
+                    active: 'yes',
+                    inactive: 'no',
+                    initial: false,
+                    name: 'overwrite',
+                },
+                {
+                    type: (_, { overwrite }) => {
+                        if (overwrite !== false) return null;
+                        throw new Error(`${red('✖')} Operation cancelled`);
+                    },
+                    name: 'overwriteChecker',
+                },
+                {
+                    type: () =>
+                        isValidPackageName(getProjectName()) ? null : 'text',
+                    name: 'packageName',
+                    message: reset('Package name:'),
+                    initial: () => toValidPackageName(getProjectName()),
+                    validate: (dir) =>
+                        isValidPackageName(dir) || 'Invalid package.json name',
+                },
+                {
+                    type: 'toggle',
+                    name: 'useTypeScript',
+                    message: 'Use TypeScript? ',
+                    active: 'yes',
+                    inactive: 'no',
+                    initial: false,
+                },
+            ],
+            {
+                onCancel: () => {
+                    throw new Error(`${red('✖')} Operation cancelled`);
+                },
+            }
+        );
+    } catch (err: any) {
+        console.log(err.message);
+        return;
+    }
 
-	const { overwrite, packageName, useTypeScript } = result;
-	const root = path.join(cwd, targetDir);
-	const excludePatterns = useTypeScript
-		? [/.*\.js$/i]
-		: [/.*\.ts$/i, 'tsconfig.json'];
+    const { overwrite, packageName, useTypeScript } = result;
+    const root = path.join(cwd, targetDir);
 
-	if (overwrite) {
-		emptyDir(root);
-	} else if (!fs.existsSync(root)) {
-		fs.mkdirSync(root, { recursive: true });
-	}
+    if (overwrite) {
+        emptyDir(root);
+    } else if (!fs.existsSync(root)) {
+        fs.mkdirSync(root, { recursive: true });
+    }
 
-	console.log(`\nScaffolding project into ${root}...`);
+    console.log(`\nScaffolding project into ${root}...`);
 
-	const templateDir = path.resolve(
-		fileURLToPath(import.meta.url),
-		'../..',
-		`template`
-	);
+    const templateDir = path.resolve(
+        fileURLToPath(import.meta.url),
+        '../..',
+        `template-${useTypeScript ? 'ts' : 'js'}`
+    );
 
-	const write = (file: string, content?: string) => {
-		if (matchesPatterns(file, excludePatterns)) return;
-		const targetPath = path.join(
-			root,
-			file === '_gitignore' ? '.gitignore' : file
-		);
-		content
-			? fs.writeFileSync(targetPath, content)
-			: copy(path.join(templateDir, file), targetPath, excludePatterns);
-	};
+    const write = (file: string, content?: string) => {
+        const targetPath = path.join(
+            root,
+            file === '_gitignore' ? '.gitignore' : file
+        );
+        content
+            ? fs.writeFileSync(targetPath, content)
+            : copy(path.join(templateDir, file), targetPath);
+    };
 
-	const files = fs.readdirSync(templateDir);
-	for (const file of files) {
-		if (file === 'package.json') continue;
-		write(file);
-	}
+    const files = fs.readdirSync(templateDir);
+    for (const file of files) {
+        if (file === 'package.json') continue;
+        write(file);
+    }
 
-	const pkg = JSON.parse(
-		fs.readFileSync(path.join(templateDir, `package.json`), 'utf-8')
-	);
-	pkg.name = packageName || getProjectName();
-	pkg.source = `./src/index.${useTypeScript ? 'ts' : 'js'}`;
-	write('package.json', JSON.stringify(pkg, null, 2));
+    const pkg = JSON.parse(
+        fs.readFileSync(path.join(templateDir, `package.json`), 'utf-8')
+    );
+    pkg.name = packageName || getProjectName();
+    pkg.source = `./src/index.${useTypeScript ? 'ts' : 'js'}`;
+    write('package.json', JSON.stringify(pkg, null, 2));
 
-	console.log(
-		`\n${green('Success!')} Created microbundle project at ${yellow(
-			root
-		)}:\n`
-	);
-	console.log(`Now run:\n`);
-	if (root !== cwd) {
-		console.log(`  cd ${path.relative(cwd, root)}`);
-	}
-	console.log('  npm install');
-	console.log();
+    console.log(
+        `\n${green('Success!')} Created microbundle project at ${yellow(
+            root
+        )}:\n`
+    );
+    console.log(`Now run:\n`);
+    if (root !== cwd) {
+        console.log(`  cd ${path.relative(cwd, root)}`);
+    }
+    console.log('  npm install');
+    console.log();
 })().catch((e) => console.error(red(e)));
