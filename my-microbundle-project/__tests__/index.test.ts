9ecde76b2e875f55879462c943522de7713fb382
Vincent Sjögren <vincent.sjogren@bitlog.se>
2024-06-30 16:22:52 +0200

    updated dependencies and removed redundant files

--- my-microbundle-project/__tests__/index.test.ts
+++
@@ -1,8 +0,0 @@
-import { describe, expect, test } from '@jest/globals';
-import {add} from "../src";
-
-describe("sample test", () => {
-    test("1 + 2 = 3", () => {
-        expect(add(1,2)).toBe(3);
-    })
-})
--- my-microbundle-project/src/index.ts
+++
@@ -1,7 +0,0 @@
-/**
- * @vinsjo/create-microbundle example function
- * replace with your own code :)
- */
-export function add(...numbers: number[]) {
-	return numbers.reduce((sum, current) => sum + current, 0);
-}
